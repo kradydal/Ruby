be1a16c6d717ce223a8c33ff8ae554cdfde1f2d7
Katarzyna Kobierska <kkobierska@gmail.com>
2015-10-05 13:32:39 +0200

    add new methods to enumerable

--- Odin/enumerable_method.rb
+++ Odin/enumerable_method.rb
@@ -16,10 +16,9 @@ module Enumerable
   end
 
   def my_each_with_index
-    return self unless block_given?
     index = 0
-    self.my_each do |element|
-      yield(element, index)
+    self.my_each do |i|
+      yield(i, index)
       index += 1
     end
     self
@@ -27,13 +26,49 @@ module Enumerable
 
   def my_select
     result = []
-    self.my_each {|element| result << element if yield(element)}
+    self.my_each {|i| result << i if yield(i)}
+    result
+  end
+
+  def my_all?
+    self.my_each {|i| return false unless yield(i)}
+    true
+  end
+
+  def my_any?
+    self.my_each {|i| return true if yield(i)}
+    false
+  end
+
+  def my_none?
+    self.my_each {|i| return false if yield(i)}
+    true
+  end
+
+  def my_count
+    result = 0
+    if block_given?
+      self.my_each {|i| result += 1 if yield(i)}
+    else
+      self.my_each {|i| result += 1}
+    end
     result
   end
 end
 
-array = [1,4,6]
+array = [1,4,6,4]
+array2 = []
 
-puts array.my_each2 {|x| x}
-puts array.my_each {|x| x}
-puts array.my_each_with_index {|n, i| array[i] = n }
+# puts array.my_each2 {|x| x}
+# puts array.my_each {|x| x}
+# puts array.my_each_with_index {|n, i| array[i] = n }
+# puts array.my_select {|x| x.even? }
+# puts array.my_all? {|x| x > 0 } #should be true
+# puts array.my_all? {|x| x < 1 } #should be false
+# puts array.my_any? {|x| x < 4} #should be true
+# puts array.my_any? {|x| x < 0} #should be false
+# puts array.my_none? {|x| x > 4} #should be false
+# puts array.my_none? {|x| x < 0} #should be true
+puts array2.my_count
+puts array.count(4)
+puts array.count {|x| x > 2}

f2b7efdd740fc171a529e82b336a3536bb781aaf
Katarzyna Kobierska <kkobierska@gmail.com>
2015-10-06 17:53:08 +0200

    add missing methods, still not working

--- Algorytmy/tic_tac_toe.rb
+++ Algorytmy/tic_tac_toe.rb
@@ -22,15 +22,8 @@ module TicTacToe
   class Board
     attr_reader :grid
 
-    def initialize(input)
-      @grid = input.fetch(:grid)
-    end
-
-    private
-
-    # it fills board with empty Cells
-    def default_grid
-      Array.new(3) {Array.new(3) {Cell.new}}
+    def initialize(input = {})
+      @grid = input.fetch(:grid, default_grid)
     end
 
     # ask for value for specific Cell
@@ -47,6 +40,19 @@ module TicTacToe
       return :winner if winner?
       return :draw if draw?
       false
+    end
+
+    def formatted_grid
+      grid.each do |row|
+        puts row.map {|cell| cell.value.empty? ? "_" : cell.value}.join(" ")
+      end
+    end
+
+    private
+
+    # it fills board with empty Cells
+    def default_grid
+      Array.new(3) {Array.new(3) {Cell.new}}
     end
 
     def draw?
@@ -105,5 +111,64 @@ module TicTacToe
       @board = board
       @current_player, @other_player = players.shuffle
     end
+
+    def switch_players
+      @current_player, @other_player = @other_player, @current_player
+    end
+
+    def solicit_move
+      "#{current_player.name}: Enter a number between 1 and 9"
+    end
+
+    def get_move(human_move = gets.chomp)
+      human_move_to_coordinate(human_move)
+    end
+
+    def game_over_message
+      return "#{current_player.name} won!" if board.game_over == :winner
+      return "The game ended in a tie" if board.game_over == :draw
+    end
+
+    def play
+      puts "#{current_player.name} has randomly been selected as the first player"
+      while true
+        board.formatted_grid
+        puts ""
+        puts solicit_move
+        x, y = get_move
+        board.set_cell(x,y, current_player.color)
+        if board.game_over
+          puts game_over_message
+          board.formatted_grid
+          return
+        else
+          switch_players
+        end
+      end
+    end
+
+    private
+
+    def human_move
+      mapping = {
+        "1" => [0,0],
+        "2" => [1,0],
+        "3" => [2,0],
+        "4" => [0,1],
+        "5" => [1,1],
+        "6" => [2,1],
+        "7" => [0,2],
+        "8" => [1,2],
+        "9" => [2,2]
+      }
+      mapping[human_move]
+    end
+
   end
 end
+
+puts "Welcome to tic tac toe"
+bob = TicTacToe::Player.new({color: "X", name: "bob"})
+frank = TicTacToe::Player.new({color: "O", name: "frank"})
+players = [bob, frank]
+TicTacToe::Game.new(players).play

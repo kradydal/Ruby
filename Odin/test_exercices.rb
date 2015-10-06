2fcc30ba4b71c59b8f5744ec858cbd6af76bead8
Katarzyna Kobierska <kkobierska@gmail.com>
2015-10-06 12:47:14 +0200

    create document for code quizzes from odin

---
+++ Odin/test_exercices.rb
@@ -0,0 +1,86 @@
+class Person
+  def initialize(age)
+    @age = age
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
+  def age
+    @age
+  end
+end
+
+# andy = Person.new(42)
+# puts andy.age
+# puts andy.age=(33)
+# puts andy.age
+
+##########################
+
+
+module MathHelper
+  def multiply_by_two(arg)
+    arg * 2
+  end
+end
+
+class Homework
+  include MathHelper
+end
+
+# math = Homework.new
+# puts math.multiply_by_two(9)
+
+##########################
+
+class BasballPlayer
+  def initialize(hits,walks,at_bats)
+    @hits = hits.to_f
+    @walks = walks.to_f
+    @at_bats = at_bats.to_f
+  end
+
+  def batting_avg
+    @hits/@at_bats
+  end
+
+  def one_base_percentage
+    (@hits + @walks)/@at_bats
+  end
+end
+
+# andrew = BasballPlayer.new(34,22,80)
+# puts andrew.batting_avg
+# puts andrew.one_base_percentage
+
+##########################
+
+class Person2
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def full_name
+    @first_name + " " + @last_name
+  end
+end
+
+# grazka = Person2.new("grazynka","kowalska")
+# puts grazka.first_name
+# puts grazka.last_name
+# puts grazka.full_name
+
+##########################
+
+

c7e44d2fd1effc9d4cfa787e956d58f3f3852313
Katarzyna Kobierska <kkobierska@gmail.com>
2015-10-07 12:40:46 +0200

    finish Game class and create class Player

--- Algorytmy/tic_tac_toe2.rb
+++ Algorytmy/tic_tac_toe2.rb
@@ -56,5 +56,22 @@ module TicTacToe
     def turn_num
       10 - free_positions.size
     end
+
+    def print_board
+      col_separator, row_separator = " | ","--+--+--"
+      label_for_position = lambda{|position| @board[position] ? @board[position] : position}
+      row_for_display = lambda{|row| row.map(&label_for_position).join(col_separator)}
+      row_positions = [[1,2,3], [4,5,6], [7,8,9]]
+      rows_for_display = row_positions.map(&row_for_display)
+      puts rows_for_display.join("\n" + row_separator + "\n")
+    end
+  end
+
+  class Player
+    attr_reader :marker
+
+    def initialize(game, marker)
+      @game = game
+      @marker = marker
   end
 end
